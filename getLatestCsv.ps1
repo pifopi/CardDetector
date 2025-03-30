b80460618c5c8b448ac5b1592815f78d7116ae78
pifopi <dottel.gael@gmail.com>
2025-03-30 01:35:07 +0100

    Update script

--- getLatestCsv.ps1
+++ getLatestCsv.ps1
@@ -3,6 +3,7 @@ $pageIds = @('1184276413', '1894219008', '48450441', '83947440', '1946812712')
 
 
 
+Push-Location $PSScriptRoot
 $outputFolder = "config"
 Foreach ($pageId in $pageIds) {
     $URL = "https://docs.google.com/spreadsheets/d/$documentId/export?format=csv&gid=$pageId"
@@ -18,3 +19,4 @@ Foreach ($pageId in $pageIds) {
 }
 
 Write-Host "Download process complete." -ForegroundColor Green
+Pop-Location
